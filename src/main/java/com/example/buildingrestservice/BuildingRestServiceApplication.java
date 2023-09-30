d870c27633a7897bab72a1d635167dededcd419e
naveen <naveengadamsetty442@gmail.com>
2023-09-30 14:20:00 +0530

    Initial commit

---
+++ src/main/java/com/example/buildingrestservice/BuildingRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buildingrestservice/BuildingRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildingrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
